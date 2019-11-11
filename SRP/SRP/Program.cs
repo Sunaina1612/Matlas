6a522550a7dad731cd4a5abe1c6635870a53d848
Sunaina1612 <57223222+Sunaina1612@users.noreply.github.com>
2019-11-11 12:30:13 +0530

    Add files via upload

---
+++ SRP/SRP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SRP
+{
+    class DataAccess
+    {
+        public static void InsertData()
+        {
+            Console.WriteLine("Data inserted into database successfully");
+        }
+    }
+    // Logger class is only responsible for logging related operations 
+    class Logger
+    {
+        public static void WriteLog()
+        {
+            Console.WriteLine("Logged Time:" + DateTime.Now.ToLongTimeString() + " Log  Data insertion completed successfully");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
